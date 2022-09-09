09e922bd750695e17fb55f648aab4eb179385d13
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2022-09-09 10:37:23 +0700

    commit1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿class Program{
+
+    static void Main (string[] args){
+
+
+        int result;
+
+        result = Converttoint();
+        Console.WriteLine(result);
+        
+       
+
+
+
+
+    }
+
+    static int Converttoint() {
+
+        string a;
+        int input;
+
+        Console.Write("Score = ");
+        a = Console.ReadLine(); 
+        input = Int32.Parse(a);
+
+        return input;
+
+    }
+
+
+
+
+    
+
+    
+
+
+
+
+
+}

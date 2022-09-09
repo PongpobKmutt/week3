b50d510046e91a14ee41ea62b0d6f32bdc3f8d56
64120501043 <sattaphon.mo@mail.kmutt.ac.th>
2022-09-09 10:43:34 +0700

    abc\

---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿public class user{
+    public string InuputName(){
+        Console.Write("Input Name : ");
+        return Console.ReadLine();
+    }
+    public string InuputSurname(){
+        Console.Write("Input Surname : ");
+        return Console.ReadLine();
+    }
+    public string InputNumber(){
+        Console.Write("Input Number : ");
+        return Console.ReadLine();
+    }
+    static void PrintInfoGrade(){
+        Console.WriteLine("Name : ",InputName);
+        Console.WriteLine("Surname : ",InputSurname);
+        Console.WriteLine("Number : ",InputNumber);
+        Console.WriteLine("Score : ");
+        Console.WriteLine("Grade : ");
+    }
+
+
+    
+}
